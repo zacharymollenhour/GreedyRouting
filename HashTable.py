0b280aea84c3c526085fe335bae4eca60354c923
Zachary Mollenhour <zacharymollenhour@Zacharys-MacBook-Air.local>
2021-01-13 01:39:24 -0600

    Finished Hash Table

--- HashTable.py
+++ HashTable.py
@@ -4,122 +4,125 @@
     We will utilize a linked list to store indexes for each element of the buckets array
     This linkedlist will provide us with key for a given index (node)
     Zachary Mollenhour WGU Student Number: 001462017
-    """
+"""
 
+'''Develop a hash table, without using any additional libraries or classes, with an insertion function that takes the following components as input and inserts the components into the hash table:
+    •  package ID number
+    •  delivery address
+    •  delivery deadline
+    •  delivery city
+    •  delivery zip code
+    •  package weight
+    •  delivery status (e.g., delivered, in route)
+'''
+import csv
 
-class LNode(object):
+class Node(object):
     '''Used for storing linkedlist of nodes that contain the objects stored at each index'''
-
-    def __init__(self, key):
+    def __init__(self, key, value):
+        #self.nodeList = []
         self.key = key
-        self.next = None
-        self.val = None
+        self.value = value
 
 
 class HashTable:
-
     def __init__(self):
         '''Define the hash table size, capacity (number of elements that have been inserted), and buckets (the internal array, storing each inserted value in a bucket based on key)'''
         self.size = 0
-        self.bucket = [None] * self.capacity
-        self.capacity = INITIAL_CAPACITY
+        self.capacity = 10
+        self.list = []
+        #Initialize all to empty values
+        for _ in range(self.capacity):
+            self.list.append([])
+        
+    def hash(self,key):
+        '''Hash Function to calculate the index where a value can be inserted into'''
+        return int(key) % len(self.list)  
 
-    def hashing_func(self, key):
-        hashCount = 0
-
-        # Loop through the characters wihtin the key paramater
-        for indexvar, currentchar in enumerate(key):
-            # Through each iteration, we will add the index and length of the key ^ current char code
-            hashCount += (indexvar=len(key)) ** ord(c)
-
-            # Now we will perform modulus on the hashcount to keep the hashcount within the range [0, self.capacity - 1]
-            hashCount = hashCount % self.capacity
-        return hashCount
 
     def insert(self, key, value):
         '''Function that handles insertion of a node into a empty bucket (if index is empty) 
            Or if the index is not empty (collision occurred) iterate to the end of the list and add a new node there'''
 
-        self.size += 1  # Start by incrementing the size
-        index = self.hash(key)  # compute the index of the passed in key
-        # Go to the corresponding node for that hash
-        hashNode = self.bucket[index]
+        #Generate index value
+        bucket = self.hash(key)
 
-        # Now we must check if the node is empty or not
-        if hashNode is None:
-            # If the node is empty, we will create a node, add it to the bucket, and return back
-            self.bucket = [index] = Node(key, value)
+        #Set the key and value pair together for appending to list
+        key_value = [key, value]
 
-            return
+        #heck if values exist in the list for the determined index
+        if self.list[bucket] == None:
+            #If not we can add the value to the list at the determined index
+            self.list[bucket] = list([key_value])
+            return True
+        else:
+            #Other wise, we will find the determined index and add it to the second [1] element spot
+            for pair in self.list[bucket]:
+                if pair[0] == key:
+                    pair[1] = key_value
+                    return True
 
-        # If the hashNode was not empty, a collision occured
-        # We must iterate to the end of hte linkedlist at that specific index
-
-        previous = hashNode
-        while hashNode is not None:
-            # Keep iterating until we get empty slot.
-            # Use previous for iterations and keeping track of location
-            previous = hashNode
-            hashNode = node.next
-
-        # Once a empty slot is located, we can now add a node to the list with provided key and value
-        previous.next = Node(key, value)
+            #Append it to the list
+            self.list[bucket].append(key_value)
+            return True
 
     def find(self, key):
         '''Function that handles the retrieving of data from the hash table'''
+        bucket = self.hash(key)
 
-        # Start by computing the hash tables index for the provided key
-        index = self.hash(key)
-
-        # Now we can take the index and go to the specific bucket for that index
-        hashNode = self.bucket[index]
-
-        # Traverse the linkedlist for the hashnode
-        while hashNode is not None and hashNode.key != key:
-            hashNode = hashNode.next
-
-        # Now that we have either found the requested key/value pairs correlating node or the node is equal to None(if node does not exist)
-        if hashNode is None:
-            #The requested node doesnt exist
-            return None
-        
-        else:
-            #Found requested node
-            return hashNode.value
+        #Check if there is a item at the determined index
+        if self.list[bucket] != None:
+            for pair in self.list[bucket]:
+                if pair[0] == key:
+                    #return value of located key
+                    return pair[1]
+        return None
 
     def delete(self,key):
         '''Function used to delete a node from a linkedlist and return the nodes value'''
 
-        #Start by computing the hash key to determine the index
-        index = self.hash(key)
-        hashNode = self.bucket[index]
-        previous = None
+        #Start by computing the hash key to determine the index using hashFunction
+        bucket = self.hash(key)
 
-        #Iterate through buckets to find the requested node
-        while hashNode is not None and hashNode.key != key:
-            previous = hashNode
-            hashNode = hashNode.next
+        #Check if the list contains any elements at the index
+        if self.list[bucket] is None:
+            return False
 
-        #At this point, we have either found the requested node or the node is not existent
-        if hashNode is None:
-            #Node key does not exist
-            return None
-        
-        else:
-            #Node key was found
-            #Decrement hash table size by one
-            self.size -= 1
+        #Otherwise, loop through the range of the length of the list at given bucket
+        for i in range(0, len(self.list[bucket])):
+            #Look for key that was passed in 
+            if self.list[bucket][i][0] == key:
+                #delete the value for passed in key
+                self.list[bucket].pop(i)
+                return True
+        return False
 
-            deleteResult = hashNode.value
+def readCSVData(filepath):
+    '''We must create a object of above class and read in the WGUPS Package File'''
 
-            #Delete element from linked list
-            if previous is None:
-                hashNode = None
-            
-            else:
-                previous.next = previous.next.next
+    #Create a HashTable Class Object
+    packageObject = HashTable()
 
-            #Return the deleted node
-            return deleteResult
+    #Open Excel File and read in content
+    with open(filepath) as WGUPackageFile:
+        #Initiate a csv reader instance
+        package_data = csv.reader(WGUPackageFile)
 
-        
+        #Automatically skip the header of the file
+        next(package_data, None)
+
+        #Loop through each row and read data into a list
+        for item in package_data:
+            #First inserts the package id item[0] and then the remainding data (all of package data)
+            packageObject.insert(int(item[0]), item)
+    print(packageObject.find(10))
+    #print(packageObject.find(10))
+    #if(packageObject.find(10) != None):
+        #print("WOW")
+    #Return Object
+    return packageObject
+
+#Call readCSVData Function with given package name
+readCSVData("WGUPSPackageFile.csv")
+
+#Hashtable
